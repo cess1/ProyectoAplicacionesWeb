12ee18cebe2eeab551fd1f693c42f99714e70198
Cesar Camacho <cescamacho94@gmail.com>
2023-11-11 15:09:36 -0500

    proyecto

---
+++ poryectoSemana11/src/main/java/org/cibertec/edu/pe/PoryectoSemana11Application.java
@@ -0,0 +1,13 @@
+package org.cibertec.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoryectoSemana11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoryectoSemana11Application.class, args);
+	}
+
+}
---
+++ poryectoSemana11/src/test/java/org/cibertec/edu/pe/PoryectoSemana11ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cibertec.edu.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoryectoSemana11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
